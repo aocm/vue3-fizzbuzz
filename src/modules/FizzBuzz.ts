607045a02af73aa2edd7f797147571aef84cd8f3
Kazuki Aoshima <aocm.dev@gmail.com>
2021-03-28 09:52:45 +0900

    FizzBuzzメイン機能の完成（テスト込み）

--- src/modules/FizzBuzz.ts
+++ src/modules/FizzBuzz.ts
@@ -2,9 +2,19 @@
 class fizzbuzz {
   constructor() { }
 
-  static convert(num: number){
-    return String(num)
+  static convert(num: number): string{
+    if (num % 15 === 0) {
+      return "FizzBuzz";
+    }
+    if (num % 3 === 0) {
+      return "Fizz";
+    }
+    if (num % 5 === 0) {
+      return "Buzz";
+    }
+    return String(num);
   }
+
 }
 
 export default fizzbuzz
--- tests/utils/fizzbuzz.spec.ts
+++ tests/utils/fizzbuzz.spec.ts
@@ -1,14 +1,37 @@
 import fizzbuzz from '@/modules/FizzBuzz';
 
-describe('FizzBuzz', () => {
-  describe('数を文字列に変換する', () => {
+describe('Fizz Buzz 数列と変換規則を扱うfizzbuzzクラス', () => {
+  beforeEach( () => {
+    // 前準備処理をここにまとめる
+  })
 
-    it('1を渡すと文字列1を返す', () => {
-      expect(fizzbuzz.convert(1)).toBe('1')
+  describe('数を文字列に変換する', () => {
+    describe('3の倍数のときは数の代わりに「Fizz」に変換する', () => {
+      test('3を渡すと文字列"Fizz"を返す', () => {
+        // 実行 & 検証
+        expect(fizzbuzz.convert(3)).toBe("Fizz");
+      });
     })
 
-    it('2を渡すと文字列2を返す', () => {
-      expect(fizzbuzz.convert(2)).toBe('2')
+    describe('5の倍数のときは数の代わりに「Buzz」に変換する', () => {
+      test('5を渡すと文字列"Buzz"を返す', () => {
+        // 実行 & 検証
+        expect(fizzbuzz.convert(5)).toBe("Buzz");
+      });
+    })
+
+    describe('3と5両方の倍数のときは数の代わりに「FizzBuzz」に変換する', () => {
+      test('15を渡すと文字列"FizzBuzz"を返す', () => {
+        // 実行 & 検証
+        expect(fizzbuzz.convert(15)).toBe("FizzBuzz");
+      });
+    })
+
+    describe('そのほかの数にの時はそのまま出力する', () => {
+      test('1を渡すと文字列"1"を返す', () => {
+        // 実行 & 検証
+        expect(fizzbuzz.convert(1)).toBe("1");
+      });
     })
   })
 })
