1a4cf0c9d46b0092e84e93c531239e72cf414535
Kazuki Aoshima <aocm.dev@gmail.com>
2021-04-06 08:21:33 +0900

    lintfix

--- src/components/FizzBuzz.stories.js
+++ src/components/FizzBuzz.stories.js
@@ -1,23 +1,23 @@
-import FizzBuzz from './FizzBuzz.vue';
+import FizzBuzz from './FizzBuzz.vue'
 
 export default {
   title: 'Component/FizzBuzz',
   component: FizzBuzz,
   argTypes: {
-  },
-};
+  }
+}
 
 const Template = (args) => ({
   // Components used in your story `template` are defined in the `components` object
   components: { FizzBuzz },
   // The story's `args` need to be mapped into the template through the `setup()` method
-  setup() {
-    return { args };
+  setup () {
+    return { args }
   },
   // And then the `args` are bound to your component with `v-bind="args"`
-  template: '<FizzBuzz v-bind="args" />',
-});
+  template: '<FizzBuzz v-bind="args" />'
+})
 
-export const Default = Template.bind({});
+export const Default = Template.bind({})
 Default.args = {
-};
+}
--- src/components/Title.stories.js
+++ src/components/Title.stories.js
@@ -1,25 +1,24 @@
-import Title from '../components/Title.vue';
+import Title from '../components/Title.vue'
 
 export default {
   title: 'Component/Title',
   component: Title,
   argTypes: {
-  },
-};
+  }
+}
 
 const Template = (args) => ({
   // Components used in your story `template` are defined in the `components` object
   components: { Title },
   // The story's `args` need to be mapped into the template through the `setup()` method
-  setup() {
-    return { args };
+  setup () {
+    return { args }
   },
   // And then the `args` are bound to your component with `v-bind="args"`
-  template: '<Title v-bind="args" />',
-});
+  template: '<Title v-bind="args" />'
+})
 
-export const Default = Template.bind({});
+export const Default = Template.bind({})
 Default.args = {
-  title: 'sample title',
-};
-
+  title: 'sample title'
+}
