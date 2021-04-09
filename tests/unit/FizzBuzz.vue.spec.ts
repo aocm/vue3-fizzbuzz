e05754356ddf08ab268481577eaa03cde46c7d97
Kazuki Aoshima <aocm.dev@gmail.com>
2021-04-09 19:43:48 +0900

    レイアウトイメージ追加＋不要なファイル削除

--- tests/unit/FizzBuzz.vue.spec.ts
+++
@@ -1,8 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import FizzBuzz from '@/components/FizzBuzz.vue'
-
-describe('FizzBuzz.vue', () => {
-  it('todo', () => {
-    expect(true).toBe(true)
-  })
-})
